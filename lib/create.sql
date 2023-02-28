8df77a535767d92b632d818fc0bbd3524d9196b5
NatePennebaker <npenne1@aol.com>
2023-02-28 13:58:19 -0500

    completed

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER, 
+    sex INTEGER,
+    color INTEGER,
+    temperament TEXT,
+    alive INTEGER
+)
---
+++ lib/insert.sql
@@ -0,0 +1,99 @@
+-- CREATE TABLE bears (
+--     id INTEGER PRIMARY KEY,
+--     name TEXT,
+--     age INTEGER, 
+--     sex INTEGER,
+--     color INTEGER,
+--     temperament TEXT,
+--     alive INTEGER
+-- )
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+-- VALUES (1, "Mr. Chocolate", 40, "M" "Brown", "Silly", 1)  
+
+INSERT INTO bears
+VALUES 
+    (
+        1, 
+        "Mr. Chocolate",
+        40,
+        "M",
+        "Brown",
+        "Silly",
+        1
+    ),
+
+    (
+        2, 
+        "Rowdy", 
+        12,
+        "M",
+        "Brown",
+        "Rowdy",
+        1
+    ),
+    (
+        3, 
+        "Tabitha", 
+        12,
+        "F",
+        "Polar",
+        "Rowdy",
+        0
+    ),
+    
+    (
+        4, 
+        "Sergeant Brown", 
+        12,
+        "M",
+        "Brown",
+        "Rowdy",
+        1
+    ),
+
+    (
+        5, 
+        "Melissa", 
+        12,
+        "F",
+        "Brown",
+        "Rowdy",
+        1
+    ),
+    (
+        6, 
+        "Grinch", 
+        12,
+        "M",
+        "Black",
+        "Rowdy",
+        0
+    ),
+
+    (
+        7, 
+        "Wendy", 
+        12,
+        "F",
+        "Grizzly",
+        "Rowdy",
+        1
+    ),
+
+    (
+        8, 
+        NULL, 
+        12,
+        "F",
+        "Koala",
+        "Rowdy",
+        1
+    )
